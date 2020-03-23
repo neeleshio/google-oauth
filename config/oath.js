caf7bfc0a1989a89e4d52fdd4dbb96e6f2a835f4
Neelesh Shetty <neeleshshetty01@gmail.com>
2020-03-23 19:39:52 +0530

    Removed client details

--- config/oath.js
+++ config/oath.js
@@ -1,7 +1,7 @@
 module.exports = {
     JWT_SECRET: 'livingheritageauth',
     google: {
-        clientID: '282936928854-v29ehfgpdq5jfauf012j6p7md1sutbd7.apps.googleusercontent.com',
-        clientSecret: 'fUWiOi8nwo7SzbirZ_diQpB_'
+        clientID: '',
+        clientSecret: ''
     }
 };
